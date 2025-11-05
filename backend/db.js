a479512a1737421e2aca9cef8bc8b27f30f8761a
Isabelle Vu <vu.isa04@gmail.com>
2025-11-05 12:09:10 -0500

    fix timestamps in db

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
-const { Pool } = require("pg");
+const { Pool, types } = require("pg");
 require("dotenv").config();
+
+// 1114 = timestamp without time zone, 1184 = timestamptz
+types.setTypeParser(1114, (val) => val); // keep as raw string
+types.setTypeParser(1184, (val) => val); // also keep as string if you ever use timestamptz
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
-
